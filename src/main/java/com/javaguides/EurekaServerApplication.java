59f9d82b983920e6a2bce2536385933b67ee4b6d
harshalmayee <harshalmayee1288@gmail.com>
2020-05-23 18:08:03 +0530

    Eureka Server Project

---
+++ src/main/java/com/javaguides/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.javaguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+		System.out.println("Calling Eureka Server...");
+	}
+
+}
